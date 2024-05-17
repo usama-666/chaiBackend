e07707cae08d3d4afdbc1359e9a7dee470e7a2ec
usama-666 <ejazali8582@gmail.com>
2024-05-17 11:55:23 +0500

    MongoDb Connection Atlas

---
+++ src/db/index.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv"
+
+import mongoose from "mongoose"
+import { DB_NAME } from "../constants.js"
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${DB_NAME}`
+    )
+    console.log(`
+    MongoDB Connected !!
+    DB HOST :${connectionInstance.Connection.host} 
+    `)
+  } catch (error) {
+    console.log("MONGODB Connection Failed :: ", error)
+    throw error
+    process.exit(1)
+  }
+}
+
+export default connectDB
--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log("Hello acken dis runnig ");
+import dotenv from "dotenv"
+import express from "express"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+  path: "./.env",
+})
+
+const app = express()
+
+connectDB()
+
+app.listen(process.env.PORT, () => {
+  console.log(`Applciation Running on Port :: ${process.env.PORT}`)
+})
+
+//Connecting DB index.js
+/*
+;(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    app.on("error", (error) => {
+      console.log("ERROR Occured :: index.js", error)
+    })
+  } catch (error) {
+    console.log("error :: catch ", error)
+    throw error
+  }
+})()
+*/
